c9ec03c7ada0cae2382f36e32210742e6bf889c1
qarmin <mikrutrafal@protonmail.com>
2022-02-07 15:09:44 +0100

    AF

--- Project/src/main.rs
+++ Project/src/main.rs
@@ -1,9 +1,11 @@
 mod ziemniak;
 
-use crate::ziemniak::execute_things;
+use crate::ziemniak::{execute_things, gget_droptarget};
+use gdk4::DragAction;
 use gtk4::prelude::*;
+use gtk4::DropTarget;
 
-const CRASHES: u64 = 0;
+const CRASHES: u64 = 1;
 
 fn main() {
     let application = gtk4::Application::builder().build();
@@ -24,46 +26,51 @@ fn main() {
 }
 
 fn crashes() {
-    gtk4::SearchEntry::new().emit_stop_search();
-    gtk4::SearchEntry::new().emit_previous_match();
-    gtk4::SearchEntry::new().emit_next_match();
-    gtk4::ListBox::new().emit_unselect_all();
-    gtk4::SearchEntry::new().emit_stop_search();
-    gtk4::SearchEntry::new().emit_previous_match();
-    gtk4::SearchEntry::new().emit_next_match();
-    gtk4::ListBox::new().emit_unselect_all();
-    gtk4::ListBox::new().emit_toggle_cursor_row();
-    gtk4::ListBox::new().emit_select_all();
-    gtk4::ListBox::new().emit_activate_cursor_row();
-    gtk4::InfoBar::new().emit_close();
-    gtk4::IconView::new().emit_select_cursor_item();
-    gtk4::IconView::new().emit_toggle_cursor_item();
-    gtk4::IconView::new().emit_unselect_all();
-    gtk4::IconView::new().emit_select_all();
-    gtk4::IconView::new().emit_activate_cursor_item();
-    gtk4::FlowBox::new().emit_unselect_all();
-    gtk4::FlowBox::new().emit_toggle_cursor_child();
-    gtk4::FlowBox::new().emit_select_all();
-    gtk4::FlowBox::new().emit_activate_cursor_child();
-    gtk4::IconView::new().drag_dest_item();
-    gtk4::EventControllerKey::new().im_context();
-    gtk4::EntryCompletion::new().insert_prefix();
+    for _i in 0..100 {
+        DropTarget::new(glib::Type::BOOL, DragAction::COPY).formats();
+        println!("a");
+    }
+
+    // gtk4::SearchEntry::new().emit_stop_search();
+    // gtk4::SearchEntry::new().emit_previous_match();
+    // gtk4::SearchEntry::new().emit_next_match();
+    // gtk4::ListBox::new().emit_unselect_all();
+    // gtk4::SearchEntry::new().emit_stop_search();
+    // gtk4::SearchEntry::new().emit_previous_match();
+    // gtk4::SearchEntry::new().emit_next_match();
+    // gtk4::ListBox::new().emit_unselect_all();
+    // gtk4::ListBox::new().emit_toggle_cursor_row();
+    // gtk4::ListBox::new().emit_select_all();
+    // gtk4::ListBox::new().emit_activate_cursor_row();
+    // gtk4::InfoBar::new().emit_close();
+    // gtk4::IconView::new().emit_select_cursor_item();
+    // gtk4::IconView::new().emit_toggle_cursor_item();
+    // gtk4::IconView::new().emit_unselect_all();
+    // gtk4::IconView::new().emit_select_all();
+    // gtk4::IconView::new().emit_activate_cursor_item();
+    // gtk4::FlowBox::new().emit_unselect_all();
+    // gtk4::FlowBox::new().emit_toggle_cursor_child();
+    // gtk4::FlowBox::new().emit_select_all();
+    // gtk4::FlowBox::new().emit_activate_cursor_child();
+    // gtk4::IconView::new().drag_dest_item();
+    // gtk4::EventControllerKey::new().im_context();
+    // gtk4::EntryCompletion::new().insert_prefix();
     // gtk4::Assistant::new().emit_escape();
-    gtk4::ListBox::new().emit_toggle_cursor_row();
-    gtk4::ListBox::new().emit_select_all();
-    gtk4::ListBox::new().emit_activate_cursor_row();
-    gtk4::InfoBar::new().emit_close();
-    gtk4::IconView::new().emit_select_cursor_item();
-    gtk4::IconView::new().emit_toggle_cursor_item();
-    gtk4::IconView::new().emit_unselect_all();
-    gtk4::IconView::new().emit_select_all();
-    gtk4::IconView::new().emit_activate_cursor_item();
-    gtk4::FlowBox::new().emit_unselect_all();
-    gtk4::FlowBox::new().emit_toggle_cursor_child();
-    gtk4::FlowBox::new().emit_select_all();
-    gtk4::FlowBox::new().emit_activate_cursor_child();
-    gtk4::IconView::new().drag_dest_item();
-    gtk4::EventControllerKey::new().im_context();
-    gtk4::EntryCompletion::new().insert_prefix();
+    // gtk4::ListBox::new().emit_toggle_cursor_row();
+    // gtk4::ListBox::new().emit_select_all();
+    // gtk4::ListBox::new().emit_activate_cursor_row();
+    // gtk4::InfoBar::new().emit_close();
+    // gtk4::IconView::new().emit_select_cursor_item();
+    // gtk4::IconView::new().emit_toggle_cursor_item();
+    // gtk4::IconView::new().emit_unselect_all();
+    // gtk4::IconView::new().emit_select_all();
+    // gtk4::IconView::new().emit_activate_cursor_item();
+    // gtk4::FlowBox::new().emit_unselect_all();
+    // gtk4::FlowBox::new().emit_toggle_cursor_child();
+    // gtk4::FlowBox::new().emit_select_all();
+    // gtk4::FlowBox::new().emit_activate_cursor_child();
+    // gtk4::IconView::new().drag_dest_item();
+    // gtk4::EventControllerKey::new().im_context();
+    // gtk4::EntryCompletion::new().insert_prefix();
     // gtk4::Assistant::new().emit_escape();
 }
